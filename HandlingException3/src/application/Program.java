12b9ea33532f6bdfc7480ebb64627af66ffcd35e
kauã dos Santos Silva <kauassilvagithub@gmail.com>
2023-03-22 16:02:12 -0300

    create: Exercicio 3

---
+++ HandlingException3/src/application/Program.java
@@ -0,0 +1,27 @@
+/*
+ * Exercício 3
+ * ----------------------------------------
+ * Crie um programa em Java com POO para calcular a média aritmética de um conjunto
+ * de números informados pelo usuário. O programa deve solicitar ao usuário quantos
+ * números deseja informar e, em seguida, solicitar que o usuário informe cada um 
+ * dos números.
+ * 
+ * No entanto, o programa deve fazer o tratamento de exceções caso o usuário informe
+ * um valor inválido, como uma letra ou um caractere especial. Caso isso aconteça, o
+ * programa deve informar ao usuário que o valor informado é inválido e solicitar que
+ * o usuário informe um novo valor.
+ * 
+ * Ao final, o programa deve exibir a média aritmética dos números informados pelo usuário.
+ */
+
+package application;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Primeiro commit");
+		
+	}
+
+}
