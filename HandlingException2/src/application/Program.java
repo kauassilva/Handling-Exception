637504e9302deea01b863eb59c725311dc9b3adf
kauã dos Santos Silva <kauassilva1@gmail.com>
2023-03-22 14:18:44 -0300

    feat: handling exceptions

--- HandlingException2/src/application/Program.java
+++ HandlingException2/src/application/Program.java
@@ -1,10 +1,84 @@
+/*
+ * Exercício 2
+ * ----------------------------------------
+ * Suponha que você esteja desenvolvendo um sistema para uma empresa que fabrica
+ * produtos eletrônicos. Você precisa criar uma classe Produto com atributos como
+ * nome, descrição, preço, quantidade em estoque, etc. Além disso, você precisa
+ * implementar um método que permita que o usuário compre um produto, que deve diminuir
+ * a quantidade em estoque do produto.
+ * 
+ * Para fazer isso, você pode criar um método "comprar()" na classe Produto que
+ * recebe um parâmetro inteiro (a quantidade de produtos a serem comprados) e diminui
+ * a quantidade em estoque do produto em relação à quantidade comprada. No entanto,
+ * o método deve verificar se a quantidade a ser comprada é maior do que a quantidade
+ * em estoque e, nesse caso, lançar uma exceção indicando que não há estoque suficiente.
+ * 
+ * Portanto, é necessário que o desenvolvedor faça o tratamento de exceções adequado 
+ * ao implementar o método comprar().
+ */
+
 package application;
+
+import java.util.Scanner;
+
+import entities.Product;
+import exceptions.BusinessException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, World!");
+		Scanner sc = new Scanner(System.in);
+		Product pro = null;
+		
+		System.out.println("Insira os dados do produto");
+		System.out.print("Nome: ");
+		String name = sc.nextLine();
+		System.out.print("Descrição: ");
+		String description = sc.nextLine();
+		System.out.print("Preço: ");
+		double price = sc.nextDouble();
+		System.out.print("Quantidade em estoque: ");
+		int quantityStock = sc.nextInt();
+		
+		/*
+		 * Instancia a classe 'Produto' com base nos dados inseridos. Caso o usuário digite
+		 * menor ou igual a 0 (zero) no parâmetro quantityStock, deve retornar uma exceção
+		 * personalizada
+		 */
+		try {
+			pro = new Product(name, description, price, quantityStock);
+		} catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		/*
+		 * Solicita ao usuário a quantidade de produtos que deseja comprar e, em seguida,
+		 * chama o método comprar() para atualizar a quantidade em estoque. Se o usuário digitar
+		 * uma quantidade inválida, uma exceção personalizada deverá ser exibida
+		 */
+		try {
+			System.out.println();
+			System.out.printf("Quantos %s deseja comprar: ",pro.getName());
+			int quantityProduct = sc.nextInt();
+			
+			try {
+				pro.buy(quantityProduct);
+				System.out.printf("%nNova quantidade em estoque: %d",pro.getQuantityStock());
+			} catch (BusinessException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		/* Caso não seja possível instanciar o Produto o sistema não deverá mostrar nada. 
+		 * Ou então, deverá mostrar a exceção NullPointerException
+		 */
+		catch (NullPointerException e) {
+			// Cannot invoke "entities.Product.getName()" because "pro" is null
+			//System.out.println(e.getMessage());
+		}
+		
+		sc.close();	
 		
 	}
 
---
+++ HandlingException2/src/entities/Product.java
@@ -0,0 +1,83 @@
+package entities;
+
+import exceptions.BusinessException;
+
+public class Product {
+	
+	private String name;
+	private String description;
+	private Double price;
+	private Integer quantityStock;
+	
+	public Product() {
+	}
+
+	public Product(String name, String description, Double price, Integer quantityStock) throws BusinessException {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		setQuantityStock(quantityStock);
+	}
+
+	/*
+	 * Getters / Setters
+	 * ========================================
+	 */
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantityStock() {
+		return quantityStock;
+	}
+
+	public void setQuantityStock(Integer quantityStock) throws BusinessException {
+		validateStock(quantityStock);
+		this.quantityStock = quantityStock;
+	}
+	
+	/*
+	 * Main methods
+	 * ========================================
+	 */
+	public void buy(int quantityProduct) throws BusinessException {
+		validateBuy(quantityProduct);
+		quantityStock -= quantityProduct;
+	}
+	
+	private void validateBuy(int quantityProduct) throws BusinessException {
+		if (quantityProduct > getQuantityStock()) {
+			throw new BusinessException("Erro de compra: Quantidade insuficiente em estoque");
+		}
+		if (quantityProduct <= 0) {
+			throw new BusinessException("Erro de compra: Quantidade desejada do produto deve ser pelo menos 1");
+		}
+	}
+	
+	private void validateStock(int quantityStock) throws BusinessException {
+		if (quantityStock <= 0) {
+			throw new BusinessException("Erro de estoque: Quantidade em estoque deve ser pelo menos 1");
+		}
+	}
+	
+}
