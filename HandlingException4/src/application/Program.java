2ef57d3464b065701481151e17ae1db75b86b933
kauã dos Santos Silva <kauassilvagithub@gmail.com>
2023-03-24 16:28:49 -0300

    create: HandlingException4

---
+++ HandlingException4/src/application/Program.java
@@ -0,0 +1,21 @@
+/*
+ * Exercício 4
+ * ----------------------------------------
+ * Suponha que você está criando um sistema de uma biblioteca e precisa criar uma 
+ * classe para representar um livro. A classe Livro terá um construtor que recebe 
+ * como parâmetros o título, o autor e o número de páginas do livro. Porém, você
+ * precisa garantir que o número de páginas seja um valor positivo, caso contrário,
+ * uma exceção deve ser lançada.
+ */
+
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
